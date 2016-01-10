b589a0065b8d58bdd5ceac8b34964eff90090f8b
b0ggyb33 <b0ggyb33@gmail.com>
2016-01-10 15:27:49 +0000

    uploaded all images
    middle mgw position drawn over background

--- src/main.c
+++ src/main.c
@@ -1,27 +1,42 @@
 #include <pebble.h>
 
 Window *my_window;
-static GBitmap *s_bitmap;
-static BitmapLayer *s_bitmap_layer;
+static GBitmap *s_background;
+static GBitmap *s_mgw_left, *s_mgw_middle, *s_mgw_right;
+
+static BitmapLayer *s_background_layer;
+static BitmapLayer *s_mgw_layer;
 
 void handle_init(void) {
   my_window = window_create();
 
-  GRect windowBounds = GRect(0, 0, 144, 168);
+  //GRect windowBounds = GRect(0, 0, 144, 168);
   
   // Load the resource
-  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG);
-
+  s_background = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG);
+  s_mgw_left = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MGW_LEFT);
+  s_mgw_middle = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MGW_MIDDLE);
+  s_mgw_right = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MGW_RIGHT);
+  
   // Create the BitmapLayer
   window_set_background_color(my_window,GColorLimerick);
-  s_bitmap_layer = bitmap_layer_create(GRect(0, 168-51, 144, 51));
-  // Set the correct compositing mode
-  bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpSet);
+  s_background_layer = bitmap_layer_create(GRect(0, 168-51, 144, 51));
+  s_mgw_layer = bitmap_layer_create(GRect(0, 168-51, 144, 51));
   
-  bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
-
+  // Set the correct compositing mode
+  bitmap_layer_set_compositing_mode(s_background_layer, GCompOpSet);
+  bitmap_layer_set_compositing_mode(s_mgw_layer, GCompOpSet);
+  
+  bitmap_layer_set_bitmap(s_background_layer, s_background);
+  
+  
+  //set mgw based on keys TODO
+  bitmap_layer_set_bitmap(s_mgw_layer, s_mgw_middle);
+   
+  
   // Add to the Window
-  layer_add_child(window_get_root_layer(my_window), bitmap_layer_get_layer(s_bitmap_layer));
+  layer_add_child(window_get_root_layer(my_window), bitmap_layer_get_layer(s_background_layer));
+  layer_add_child(window_get_root_layer(my_window), bitmap_layer_get_layer(s_mgw_layer));
 
   
   window_stack_push(my_window, true);
@@ -29,8 +44,12 @@ void handle_init(void) {
 
 void handle_deinit(void) {
   window_destroy(my_window);
-  gbitmap_destroy(s_bitmap);
-  bitmap_layer_destroy(s_bitmap_layer);
+  gbitmap_destroy(s_background);
+  gbitmap_destroy(s_mgw_middle);
+  gbitmap_destroy(s_mgw_left);
+  gbitmap_destroy(s_mgw_right);
+  bitmap_layer_destroy(s_background_layer);
+  bitmap_layer_destroy(s_mgw_layer);
 }
 
 int main(void) {
