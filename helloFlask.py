d6af1db0bce4a8fe803fa32de8bbf27cccdea4fe
b0ggyb33 <b0ggyb33@gmail.com>
2016-01-19 11:19:41 +0000

    made rthinkdb work with data, and putting data on screen, in crappy graphics

--- helloFlask.py
+++ helloFlask.py
@@ -1,27 +1,44 @@
 from flask import Flask, request,jsonify
 
 import rethinkdb as r
+import tabulate
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    r.connect().repl()
+    results = r.table("authors").filter(r.row["username"]=="xyz").order_by(r.desc("score")).limit(3).run()
+
+    resultsList=[["Name","Score"]]
+    for result in results:
+        resultsList.append([result['username'],result['score']])
+
+
+
+    return str(tabulate.tabulate(resultsList,headers="firstrow"))
 
 
 @app.route('/json', methods=['POST'])
 def json():
 		
-	data=request.get_json()
-	print data
-		
-	r.table("authors").insert(jsonify(data)).run(5000)
+    data=request.get_json()
+    print data
+    r.connect().repl()
+    try:
+        r.db("test").table_create("authors").run()
+    except r.ReqlOpFailedError:
+        pass
+
+    r.table("authors").insert(data).run()
 	
-	return jsonify(data)    
+    return jsonify(data)
+
 
 
 if __name__ == "__main__":
-    r.connect().repl()
+    #r.connect().repl()
     #r.db("test").table_create("authors").run()
     app.debug = True
     app.run()
