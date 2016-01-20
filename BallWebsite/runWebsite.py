55412cbb0f4baafde9fd8e0e6aa398455062ae0d
b0ggyb33 <b0ggyb33@gmail.com>
2016-01-20 21:32:01 +0000

    updated some things

--- BallWebsite/runWebsite.py
+++ BallWebsite/runWebsite.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 def hello():
     r.connect().repl()
     results = r.table("authors").order_by(r.desc("score")).limit(10).run()
+    for result in results:
+        result['username']=getNameOfWatch(result['username'])
 
     return render_template('highScores.html', scores=results)
 
@@ -23,15 +25,25 @@ def json():
         r.db("test").table_create("authors").run()
     except r.ReqlOpFailedError:
         pass
-
+    
     r.table("authors").insert(data).run()
-	
+    
     return jsonify(data)
 
+def getNameOfWatch(name):
+    try:
+        r.db("test").table_create("nameMapping").run()
+    except r.ReqlOpFailedError:
+        pass
+    
+    results = r.table("nameMapping").filter(r.row['username']==name).run()
+    for item in results:
+        print item
+    return name
 
 
 if __name__ == "__main__":
     #r.connect().repl()
     #r.db("test").table_create("authors").run()
     app.debug = True
-    app.run()
+    app.run('0.0.0.0')
